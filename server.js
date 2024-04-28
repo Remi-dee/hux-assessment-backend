87c77bad352f7ea66b7f60be8f472fd502b8eb57
Your Name <you@example.com>
2024-04-28 17:51:31 +0100

    Fix port already in use issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const contactRoutes = require("./routes/contactRoutes");
-const port = 2000  ||process.env.PORT ;
+const port =   5000  ;
 
 const corsOptions = {
   origin: true,
